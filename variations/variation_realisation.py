5013fcaa9174a46925c7247e3d3f585fdf588b73
christmasuicide <94450178+christmasuicide@users.noreply.github.com>
2021-11-17 16:35:22 +0200

    work methods_declared

---
+++ variations/variation_realisation.py
@@ -0,0 +1,14 @@
+import random
+import re
+
+class TemplateEngine:
+
+    def __init__(self, answers):
+        self.templates = answers
+        self.default_answers = [answer for (template, answer) in answers if template == ']']
+
+    def cases()
+ 
+    def get_meaning()
+
+
