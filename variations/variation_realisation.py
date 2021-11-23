21c240f11b21a876d2fecea36067062535d19b09
Rokharr <dudkataras99@gmail.com>
2021-11-23 15:19:12 +0200

    Fixed things

--- variations/variation_realisation.py
+++ variations/variation_realisation.py
@@ -7,8 +7,15 @@ class TemplateEngine:
         self.templates = answers
         self.default_answers = [answer for (template, answer) in answers if template == ']']
 
-    def cases()
- 
+    def cases(self, case, question, answer):
+        # if template contains '*' operator
+        if case.find("]") != -1:
+            case = case.replace("]", "")
+
+            # if template contains only one char along with '*', and both answer and question has this char
+            if len(case) == 1 and question.find(case) != -1 and answer.find(case) != -1:
+                return answer
+
     def get_meaning()
 
 
