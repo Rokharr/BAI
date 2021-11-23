67966fad2dad1b220be45c96c63f3e8bd8fdfb69
christmasuicide <94450178+christmasuicide@users.noreply.github.com>
2021-11-23 16:51:23 +0200

    fixed and defined methods

---
+++ variations/variation_realisation.py
@@ -0,0 +1,15 @@
+import random
+import re
+
+class TemplateEngine:
+
+    def __init__(self, answers):
+        self.templates = answers
+        self.default_answers = [answer for (template, answer) in answers if template == ']']
+
+    def cases(self, case, question, answer)
+
+    def get_meaning(self, ask)
+ 
+
+
